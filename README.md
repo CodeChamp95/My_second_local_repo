fb898102c9b18dcc33abfc02702e2d62aef47f17
Areeb Shaikh <areeb5389@gmail.com>
2024-07-21 14:31:41 +0530

    New file

---
+++ README.md
@@ -0,0 +1,2 @@
+# My_second_local_repo
+This is a repo created locally and then pushed on to the Github
---
+++ hello.py
@@ -0,0 +1,6 @@
+str_index=int(input("Enter 1 if happy and 0 if sad: "))
+if(str_index==1):
+    print("Nice! Have a good day")
+else:
+    print("Do not worry! Everything will be just fine")
+    
